1189b72ed2d534fcf4f7ab52d5b7464bbae1ce94
16bitash <entropy.ash@gmail.com>
2022-08-13 21:37:13 +0530

    Fix: Paragraph not visible in nav link and line background color

--- src/components/navigation/Navigation.jsx
+++ src/components/navigation/Navigation.jsx
@@ -1,30 +1,33 @@
-import React from 'react';
+import React from "react";
 
-import './Navigation.css';
+import "./Navigation.css";
 
 import NavLinks from "./NavLinks";
-import {menuData} from "../../utility/menuData";
+import { menuData } from "../../utility/menuData";
 
 function Navigation() {
-    return (
-        <nav className={"navigation"}>
-            <div className={"container"}>
-                <ul className={"navigation__list"}>
-                    <li className="navigation__item">
-                        <div className="navigation__mobile">
-                            <span className="line line-top"></span>
-                            <span className="line line-bottom"></span>
-                        </div>
-                    </li>
-                    {menuData.map((item) => item.icon === '' ?
-                        <NavLinks link={item.link} title={item.title} key={item.id}/> :
-                        <NavLinks link={item.link} icon={item.icon} key={item.id}/>
-                    )
-                    }
-                </ul>
+  return (
+    <nav className="navigation">
+      <div className="container">
+        <ul className="navigation__list">
+          <li className="navigation__item">
+            <div className="navigation__mobile">
+              <span className="line line-top"></span>
+              <span className="line line-bottom"></span>
             </div>
-        </nav>
-    );
+          </li>
+          {menuData.map((item) => (
+            <NavLinks
+              link={item.link}
+              title={item.title}
+              icon={item.icon}
+              key={item.id}
+            />
+          ))}
+        </ul>
+      </div>
+    </nav>
+  );
 }
 
 export default Navigation;
